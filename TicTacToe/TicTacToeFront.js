90832e219e87d0d3c0cdd37e828292334ac7b399
Noah <31552830+Kingallice@users.noreply.github.com>
2022-03-28 19:52:41 -0500

    Update TicTacToeFront.js

--- TicTacToe/TicTacToeFront.js
+++ TicTacToe/TicTacToeFront.js
@@ -135,7 +135,7 @@ function winCheck(){
 					noEmpty = false;
 			}
 		}
-		if (noEmpty && x = 'O'){
+		if (noEmpty && x == 'O'){
 			endText.innerHTML = "No Winner!";
 			winElement.hidden = false
 			return [true, null];
